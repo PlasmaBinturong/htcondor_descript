00479f07dac68a6227acb130b1e1cd6d010b6abd
GullumLuvl <yom7@hotmail.fr>
2019-09-19 15:24:10 +0200

    Include numeric codes for more Condor states. Option to skip files generating unknown errors.

--- condor_checklogs.py
+++ condor_checklogs.py
@@ -14,6 +14,14 @@ import logging
 import datetime as dt
 logger = logging.getLogger(__name__)
 
+RESET = "\033[0m"
+COLOR_SEQ = "\033[0;%dm"
+RED = COLOR_SEQ % 31
+GREEN = COLOR_SEQ % 32
+
+OK = GREEN + 'OK' + RESET
+NO = RED + 'NO' + RESET
+
 TIME_FORMAT = '%m/%d %H:%M:%S'
 
 RE_RETURN = re.compile(r'\(([^()]+) (\d+)\)')
@@ -22,19 +30,22 @@ RE_DATE = re.compile(r'0\d\d \([0-9.]+\) (\d+/\d+ \d+:\d+:\d+) ')
 #RE_STARTED = re.compile(r'001 ')
 #RE_TERM = re.compile(r'005 ')
 RE_MEM = re.compile(r'^\s*Memory \(MB\)\s+:\s*(\d+)\s+(\d+)\s+(\d+)$')
-RE_MEMUPDATE = re.compile(r'^\s*(\d+)\s+-\s+MemoryUsage of job ([A-Za-z]+)$')
+RE_MEMUPDATE = re.compile(r'^\s*(\d+)\s+-\s+MemoryUsage of job \(([A-Za-z]+)\)$')
 
 STATES = {'000 ': 'submitted',
           '001 ': 'started',
           '004 ': 'evicted',
           '005 ': 'terminated',
           '006 ': 'Image size updated',
+          '007 ': 'Shadow exception!',
           '009 ': 'aborted',
+          '012 ': 'hold',
+          '013 ': 'released',
           '022 ': 'disconnected',  # attempting to reconnect
           '024 ': 'reconnection failed'}  # disconnected too long. rescheduling job
 
 ended = set(('evicted', 'terminated', 'aborted', 'disconnected', 'reconnection failed'))
-running = set(('submitted', 'started', 'Image size updated'))
+running = set(('submitted', 'started', 'Image size updated'))  # 'hold', 'released'
 
 
 def termination_code(logfile):
@@ -47,29 +58,38 @@ def termination_code(logfile):
     memory_match = None
     memupdates = []  # All memory values at logged timepoints.
     with open(logfile) as log:
-        for line in log:
+        for lineno, line in enumerate(log, start=1):
             state_code = line[:4]
 
-            if state_code in STATES:
-                state = STATES.get(state_code)
-                date = RE_DATE.match(line).group(1)
+            try:
+                if state_code in STATES:
+                    state = STATES.get(state_code)
+                    date = RE_DATE.match(line).group(1)
 
-            if state == 'terminated':
-                m = RE_RETURN.search(line)
-                if m:
-                    termination_match = m
-                else:
+                if state == 'terminated':
+                    m = RE_RETURN.search(line)
+                    if m:
+                        termination_match = m
+                    else:
+                        m = RE_MEM.match(line)
+                        if m:
+                            memory_match = m
+                    memupdates = []
+                elif state == 'evicted':
                     m = RE_MEM.match(line)
                     if m:
                         memory_match = m
-                memupdates = []
-            elif state == 'Image size updated':
-                m = RE_MEMUPDATE.match(line)
-                if m:
-                    memupdates.append(int(m.group(1)))
-            elif state in set(('submitted', 'started')):
-                memory_match = None
-                memupdates = []
+                    memupdates = []
+                elif state == 'Image size updated':
+                    m = RE_MEMUPDATE.match(line)
+                    if m:
+                        memupdates.append(int(m.group(1)))
+                elif state in set(('submitted', 'started')):
+                    memory_match = None
+                    memupdates = []
+            except BaseException as err:
+                err.args += ("At line %d '%s'" % (lineno, line.rstrip()),)
+                raise
 
     try:
         return_type = termination_match.group(1)
@@ -82,8 +102,10 @@ def termination_code(logfile):
     try:
         memories = tuple(int(x) for x in memory_match.groups())
     except AttributeError:
-        if state == 'terminated':
-            logger.warning('Could not match the memory amounts')
+        if state in ('terminated', 'evicted'):
+            if return_value == 0:
+                logger.warning('Could not match the memory amounts: %s', logfile)
+            memories = (None,)*3
         elif memupdates:
             memories = (max(memupdates), None, None)
         else:
@@ -92,7 +114,8 @@ def termination_code(logfile):
     return (state, date, return_type, return_value) + memories
 
 
-def main(logfiles, show_all=False, terminated_only=False, memory=False, sort=False):
+def main(logfiles, show_all=False, terminated_only=False, memory=False,
+         sort=False, ignore_errors=False):
     count_failed = 0
     count_noterm = 0
     used_memory = []
@@ -102,40 +125,55 @@ def main(logfiles, show_all=False, terminated_only=False, memory=False, sort=Fal
             state, date, return_type, return_value, mem_used, _, mem_alloc = \
                     termination_code(logfile)
         except BaseException as err:
-            err.args += ("At %r" % logfile,)
-            raise
+            err.args += ("At %s" % logfile,)
+            if ignore_errors and not isinstance(err, KeyboardInterrupt):
+                logger.exception('Unknown error')
+                continue
+            else:
+                raise
         msg = None
         if state:
             if state.startswith('terminated'):
                 if memory:
                     used_memory.append(mem_used)
                     if mem_used > mem_alloc:
-                        msg = 'Exceeded allocated memory! %d > %d (MB): %s' % (
+                        msg = NO + ': exceeded allocated memory! %d > %d (MB): %s' % (
                                 mem_used, mem_alloc, logfile)
                         count_failed += 1
                     else:
-                        msg = 'OK: memory used %d <= %d memory allocated (MB): %s' % (
+                        msg = OK + ' memory used %d <= %d memory allocated (MB): %s' % (
                                 mem_used, mem_alloc, logfile)
                 else:
                     if return_value != 0:
-                        msg = "Termination with error at %s: %s %2d : %s" % (
+                        msg = NO + ": error at %s: %s %2d : %s" % (
                                 date, return_type, return_value, logfile)
                         count_failed += 1
                     elif show_all or terminated_only:
-                        msg = "OK (%s): %s" % (date, logfile)
+                        msg = OK + " (%s): %s" % (date, logfile)
             elif state in ended:
+                msg = "Condor termination"
                 if memory:
                     if mem_used is not None:
-                        msg = "Condor termination (max memory = %s (MB)): %s" % (
-                                mem_used, logfile)
+                        if mem_alloc is not None:
+                            if mem_used > mem_alloc:
+                                msg += ': '+NO+': exceeded allocated memory! %d > %d (MB): %s' % (
+                                        mem_used, mem_alloc, logfile)
+                                count_failed += 1
+                            else:
+                                msg += OK + '. Memory used %d <= %d memory allocated (MB): %s' % (
+                                        mem_used, mem_alloc, logfile)
+                        else:
+                            msg += " (max memory = %s (MB)): %s" % (mem_used, logfile)
                         used_memory.append(mem_used)
                 else:
                     msg = "Condor termination (%s at %s): %s" % (state, date, logfile)
-                count_noterm += 1
+                    count_noterm += 1
             else:
                 if not terminated_only:
                     if memory:
-                        msg = "Running (max_memory %s (MB)): %s" % (mem_used, logfile)
+                        msg = "Running (max_memory %5s (MB)): %s" % (
+                                ('-' if mem_used is None else mem_used),
+                                logfile)
                         if mem_used is not None:
                             used_memory.append(mem_used)
                     else:
@@ -146,7 +184,15 @@ def main(logfiles, show_all=False, terminated_only=False, memory=False, sort=Fal
 
         if msg:
             if sort:
-                outputs.append((dt.datetime.strptime(date, TIME_FORMAT), msg))
+                try:
+                    parsed_date = dt.datetime.strptime(date, TIME_FORMAT)
+                    outputs.append((parsed_date, msg))
+                except ValueError as err:
+                    err.args += ("On string %r" % date, "At %s" % logfile)
+                    if ignore_errors:
+                        logger.exception('Date parsing error')
+                    else:
+                        raise
             else:
                 print(msg)
     
@@ -156,7 +202,9 @@ def main(logfiles, show_all=False, terminated_only=False, memory=False, sort=Fal
 
     if memory:
         print("%d exceeded, %d not terminated (max: %d MB, total: %d)"
-              % (count_failed, count_noterm, max(used_memory), len(logfiles)))
+              % (count_failed, count_noterm,
+                  (max(used_memory) if used_memory else None),
+                  len(logfiles)))
     else:
         print("%d failed, %d not terminated (total: %d) " % (count_failed,
                                                              count_noterm,
@@ -175,6 +223,8 @@ if __name__=='__main__':
                         help='Report jobs that exceeded allocated memory.')
     parser.add_argument('-s', '--sort', action='store_true',
                         help='Sort by time')
+    parser.add_argument('-i', '--ignore-errors', action='store_true',
+                        help='Skip files that raise errors and continue')
     args = parser.parse_args()
     main(**vars(args))
 
